47739ed74def5b339e5b6daf8936edb281f67f90
sapey <alexandre.froment92@gmail.com>
2022-10-19 14:17:31 +0200

    add global navigation

--- content-app/app.vue
+++ content-app/app.vue
@@ -1,5 +1,30 @@
+<script setup>
+const { navigation } = useContent()
+</script>
 <template>
   <div>
+    <nav>
+      <ul>
+        <li v-for="nav in navigation" :key="nav._path">
+        <NuxtLink :to="nav._path">
+        {{ nav.title }}
+        </NuxtLink>
+        </li>
+      </ul>
+    </nav>
     <NuxtPage />
   </div>
 </template>
+
+<style scoped>
+  nav ul {
+    display: flex;
+    justify-content: center;
+    padding: 0;
+  }
+  nav ul li{
+    list-style: none;
+    margin: 0;
+    padding: 2px 8px;
+  }
+</style>
