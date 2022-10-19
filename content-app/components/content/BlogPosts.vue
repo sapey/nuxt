dce386ba7defa9188893bea552bcb13d399736a5
sapey <alexandre.froment92@gmail.com>
2022-10-19 14:39:11 +0200

    list blog posts

---
+++ content-app/components/content/BlogPosts.vue
@@ -0,0 +1,16 @@
+<script setup>
+const { data } = await useAsyncData(() => {
+    return queryContent('/blog/').find()
+})
+</script>
+<template>
+    <div>
+        <div v-for="post in data" :key="post._id">
+            <h2>{{ post.title }}</h2>
+            <p>{{ post.description }}</p>
+            <NuxtLink :to="post._path">
+                Read more...
+            </NuxtLink>
+        </div>
+    </div>
+</template>
